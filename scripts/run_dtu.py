ba5fbe8d36aee5178cbe4c3fce46adb3b0c55eef
Hong753 <hongjung753@gmail.com>
2026-05-14 15:21:43 +0900

    Init

--- scripts/run_dtu.py
+++ scripts/run_dtu.py
@@ -1,7 +1,7 @@
 import os
 
 # scenes = [24, 37, 40, 55, 63, 65, 69, 83, 97, 105, 106, 110, 114, 118, 122]
-scenes = [24]
+scenes = [37, 40]
 data_base_path='/workspace/colmap_scenes/DTU/dtu'
 out_base_path='/workspace/colmap_scenes/DTU/dtu_output'
 eval_path='/workspace/colmap_scenes/DTU/dtu_eval'
