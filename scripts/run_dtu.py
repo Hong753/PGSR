3150980051f1e83fb106a05126f52db66527bef9
Hong753 <hongjung753@gmail.com>
2026-05-14 16:23:11 +0900

    Init

--- scripts/run_dtu.py
+++ scripts/run_dtu.py
@@ -17,15 +17,15 @@ for scene in scenes:
     # print(cmd)
     # os.system(cmd)
 
-    # common_args = "--quiet -r2 --ncc_scale 0.5"
-    # cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python train.py -s {data_base_path}/scan{scene} -m {out_base_path}/dtu_scan{scene}/{out_name} {common_args}'
-    # print(cmd)
-    # os.system(cmd)
+    common_args = "--quiet -r2 --ncc_scale 0.5"
+    cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python train.py -s {data_base_path}/scan{scene} -m {out_base_path}/dtu_scan{scene}/{out_name} {common_args}'
+    print(cmd)
+    os.system(cmd)
 
-    # common_args = "--quiet --num_cluster 1 --voxel_size 0.002 --max_depth 5.0"
-    # cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python render.py -m {out_base_path}/dtu_scan{scene}/{out_name} {common_args}'
-    # print(cmd)
-    # os.system(cmd)
+    common_args = "--quiet --num_cluster 1 --voxel_size 0.002 --max_depth 5.0"
+    cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python render.py -m {out_base_path}/dtu_scan{scene}/{out_name} {common_args}'
+    print(cmd)
+    os.system(cmd)
 
     cmd = f"CUDA_VISIBLE_DEVICES={gpu_id} python scripts/eval_dtu/evaluate_single_scene.py " + \
           f"--input_mesh {out_base_path}/dtu_scan{scene}/{out_name}/mesh/tsdf_fusion_post.ply " + \
